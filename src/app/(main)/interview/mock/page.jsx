ab50ae6cf3095a058fa1e3302b3c5709061bf450
PAVAN-DEVMURARI <devmuraripavan556@gmail.com>
2025-08-25 22:07:07 +0530

    Resolved the path issue

--- src/app/(main)/interview/mock/page.jsx
+++ src/app/(main)/interview/mock/page.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import Quiz from "../_components/Quiz";
+import Quiz from "../_components/quiz";
 
 
 
